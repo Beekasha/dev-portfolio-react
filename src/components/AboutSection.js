54b6e249fbcb51cb55a2531142a326380fc953d1
Beekasha <brian.j.kleiner@gmail.com>
2020-11-27 17:50:23 -0500

    Add styled components

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import home1 from '../img-samples/home1.png'
 
+// styled components
+import styled from 'styled-components'
+
 const AboutSection = () => {
     return(
-        <div>
-            <div className="description">
+        <StyledAbout>
+            <StyledDescription>
                 <div className="title">
                     <div className="hide">
                         <h2>We work to make</h2>
@@ -18,12 +21,41 @@ const AboutSection = () => {
                 </div>
                 <p>Contact us for any coding projects you may have.</p>
                 <button>Contact Us</button>
-            </div>
-            <div className="image">
+            </StyledDescription>
+            <StyledImage>
                 <img src={home1} alt="random dude pic"/>
-            </div>
-        </div>
+            </StyledImage>
+        </StyledAbout>
     )
-}
+};
+
+//Styled Component
+const StyledAbout = styled.div`
+    min-height: 90vh;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 5rem 10rem;
+    color: white;
+`
+const StyledDescription = styled.div`
+    flex: 1;
+    padding-right: 5rem;
+    h2{
+        font-weight: lighter;
+    }
+`
+
+const StyledImage = styled.div`
+    flex: 1;
+    overflow: hidden;
+    img{
+        width: 100%;
+        height: 80vh;
+        object-fit: cover;
+    }
+`
+
+
 
 export default AboutSection;
