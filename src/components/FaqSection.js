4da33d45cabb57cf19bf016538af22236dd40ffc
Beekasha <brian.j.kleiner@gmail.com>
2020-11-27 17:19:21 -0500

    FAQ section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,49 @@
+
+const FaqSection = () => {
+    return(
+        <div className="faq">
+            <h2>
+                Any questions? <span>FAQ</span>
+            </h2>
+            <div className="question">
+                <h4>How do I start?</h4>
+                <div className="answer">
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. </p>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe pariatur voluptas hic asperiores quidem quaerat numquam aliquam in commodi optio reprehenderit, nemo debitis eligendi, quibusdam sunt similique accusantium, dicta aspernatur.
+                    </p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>Daily Schedule</h4>
+                <div className="answer">
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. </p>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe pariatur voluptas hic asperiores quidem quaerat numquam aliquam in commodi optio reprehenderit, nemo debitis eligendi, quibusdam sunt similique accusantium, dicta aspernatur.
+                    </p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>Payment Methods</h4>
+                <div className="answer">
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. </p>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe pariatur voluptas hic asperiores quidem quaerat numquam aliquam in commodi optio reprehenderit, nemo debitis eligendi, quibusdam sunt similique accusantium, dicta aspernatur.
+                    </p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>Products offered?</h4>
+                <div className="answer">
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. </p>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe pariatur voluptas hic asperiores quidem quaerat numquam aliquam in commodi optio reprehenderit, nemo debitis eligendi, quibusdam sunt similique accusantium, dicta aspernatur.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default FaqSection;
