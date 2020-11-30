7c3b094aebffa3283ec9b272270183eb38664a8c
Beekasha <brian.j.kleiner@gmail.com>
2020-11-29 20:22:45 -0500

    setting up React Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 //global styling
 import GlobalStyle from './components/GlobalStyle'
 import AboutUs from './pages/AboutUs'
+import ContactUs from './pages/ContactUs'
+import OurWork from './pages/OurWork'
 import Nav from './components/Nav'
+
+import { Switch, Route } from 'react-router-dom'
+
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
