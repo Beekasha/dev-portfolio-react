ac6576e2e933eac6031f05480f57b6e016cc42d7
Beekasha <brian.j.kleiner@gmail.com>
2020-12-04 22:28:26 -0500

    readme

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -4,6 +4,7 @@ import { useHistory } from 'react-router-dom'
 const MovieDetail = () => {
     const history = useHistory();
     console.log({history})
+    // console.log(history.location.pathname.slice(6, -1))
     return(
         <div>
             <h1>MovieDetail</h1>
