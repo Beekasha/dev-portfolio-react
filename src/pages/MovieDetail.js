e635cf7d0998c5cbdfa4856a5250c6a82fac5fb4
Beekasha <brian.j.kleiner@gmail.com>
2020-12-19 11:07:30 -0500

    saving individual movie in state when going to specific url

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -1,10 +1,22 @@
 import styled from 'styled-components';
 import { useHistory } from 'react-router-dom'
+import {MovieState} from '../movieState'
+import {useState, useEffect} from 'react';
 
 const MovieDetail = () => {
     const history = useHistory();
     console.log({history})
     // console.log(history.location.pathname.slice(6, -1))
+    const url = history.location.pathname;
+    const [movies,setMovies] = useState(MovieState);
+    const [movie,  setMovie] = useState(null);
+
+    //UseEffect
+    useEffect(() => {
+        const currentMovie = movies.filter((stateMovie) => stateMovie.url === url)
+        setMovie(currentMovie);
+    }, [movies, url]);
+
     return(
         <div>
             <h1>MovieDetail</h1>
